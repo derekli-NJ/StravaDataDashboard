593afbb334a379b22d2d4b98c5beff2303451ec7
Derek Li <derekgolf72@yahoo.com>
2019-07-12 23:55:58 -0400

    Changing implementation to webscraping

--- StravaApp.py
+++ StravaApp.py
@@ -1,12 +1,16 @@
 from InfoRetriever import get_race_data, get_athlete_info
-from VDOT import enter_data, read_vdot
+from VDOT import format_data, read_vdot, create_database, calculate_vdot
 
 
-access_token = "Your access token here"
+access_token = "40966e55980680825230ee14e355ce2c05672b20"
 
 
 athlete_info = get_athlete_info(access_token)
 race_data = get_race_data(access_token)
 
-enter_data(athlete_info, race_data)
+data = format_data(athlete_info, race_data)
+
+calculate_vdot(data)
+
+create_database(data)
 
--- VDOT.py
+++ VDOT.py
@@ -1,9 +1,82 @@
+import sqlite3
 import openpyxl
 from units import unit
 import units.predefined
+from stravalib import unithelper as uh
+
+import xlwings as xw
+import requests
+from bs4 import BeautifulSoup
+import json
+import brotli
+
+def format_data(athlete_info, race_data):
+    #print (race_data)
+    race_distances = { "5k" : uh.meter(5000), "8k" : uh.meter(8000), "10k" : uh.meter(10000), "12k" : uh.meter(12000), "15k" : uh.meter(15000), "1/2 Mar" : uh.meter(21097.5), "Marathon" : uh.meter(42195)}
+
+    formatted_race_data = [] 
+
+    for race in race_data:
+        for distance in race_distances:
+            if (abs(race_distances[distance] - race.distance) < 0.1 * race.distance):   
+                race.distance = race_distances[distance]
+                # Zfill adds leading 0 if string is less than 8 characters (needed formatting for post request)
+                formatted_race_data.append((race.distance, str(race.moving_time).zfill(8)))
+                #Need to add way to deal with custom length race later
+    return formatted_race_data
+    #do more here
+                
+
+def calculate_vdot(race_data):
+    
+    headers = {
+        'cookie': '__cfduid=d0b9f95405c5e2d2099437f23da0da88f1561088759; __utmc=140335664; _ga=GA1.2.1431627166.1561088760; __utmz=140335664.1561089351.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); _gid=GA1.2.1220013293.1562985965; _gat=1; __utma=140335664.1431627166.1561088760.1561089351.1562985974.3; __utmt=1; __utmb=140335664.3.9.1562985987153',
+        'origin': 'https://runsmartproject.com',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,de;q=0.8',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'referer': 'https://runsmartproject.com/calculator/',
+        'authority': 'runsmartproject.com',
+        'x-requested-with': 'XMLHttpRequest',
+    }
+    count = 0 
+    for race in race_data:
+        if count == 0:
+            data = {
+              'distance': race[0],
+              'unit': 'm',
+              'time': race[1],
+              'pace': 'empty',
+              'punit': 'mi',
+              'temp': '',
+              'tunit': 'F',
+              'alt': '',
+              'aunit': 'ft',
+              'advtype': 'temperature',
+              'predict': 'true'
+            }
+
+            #r = requests.get('https://runsmartproject.com/calculator/')
+            response = requests.post('https://runsmartproject.com/vdot/app/api/find_paces', headers=headers, data=data)
+            
+            print (brotli.decompress(response.content))
+            #r = requests.get('https://runsmartproject.com/calculator/')
+
+            soup = BeautifulSoup(response.text, "html.parser")
+            links = soup.find_all("a")
+            tag = soup.find("div", id="vdot")
+        count += 1
 
 
-def enter_data(athlete_info, race_data):
+def create_database(data): 
+    sql_create_accounts_table= """ CREATE TABLE IF NOT EXISTS race_data(
+                                         id integer PRIMARY KEY,
+                                         distance decimal,
+                                         time decimal,
+                                     ); """
+    '''
     srcfile = openpyxl.load_workbook('test.xlsm',read_only=False, keep_vba= True)#to open the excel sheet and if it has macros
 
     sheetname = srcfile.get_sheet_by_name('Daniels Tables')#get sheetname from the file
@@ -35,10 +108,10 @@ def enter_data(athlete_info, race_data):
         print ("Prompt user for heart rate max")
 
 
-    meter = unit('m')
+    uh.meter = unit('m')
 
 
-    race_distances = { "5k" : meter(5000), "8k" : meter(8000), "10k" : meter(10000), "12k" : meter(12000), "15k" : meter(15000), "1/2 Mar" : meter(21097.5), "Marathon" : meter(42195)}
+    race_distances = { "5k" : uh.meter(5000), "8k" : uh.meter(8000), "10k" : uh.meter(10000), "12k" : uh.meter(12000), "15k" : uh.meter(15000), "1/2 Mar" : uh.meter(21097.5), "Marathon" : uh.meter(42195)}
     
     filled_distance = False
     for race in race_distances:
@@ -50,12 +123,31 @@ def enter_data(athlete_info, race_data):
         sheetname['S5'] = race_data[0].distance
     
 
-    sheetname['G6'] = str('3:25:24') #Edits the race time
-
-    sheetname.cell(row=20,column=22).value = "something" #write to row 1,col 1 explicitly, this type of writing is useful to write something in loops
+    print (sheetname['J6'].value)
+    sheetname['G6'] = str(race_data[0].elapsed_time) #Edits the race time
 
     srcfile.save('test.xlsm')#save it as a new file, the original file is untouched and here I am saving it as xlsm(m here denotes macros).
 
 
+    #wb = openpyxl.load_workbook('test.xlsm' , data_only=True)
+    #data_sheet = wb['Daniels Tables']
+
+
+
+
+    #srcfile.close()
+    
+    read_vdot()
+    '''
+
 def read_vdot():
+    #srcfile = openpyxl.load_workbook('test.xlsm', read_only= True)#to open the excel sheet and if it has macros
+
+    srcfile = openpyxl.load_workbook('test.xlsm', data_only = True)#to open the excel sheet and if it has macros
+    #temp_sheetname = temp_srcfile.get_sheet_by_name('Daniels Tables')#get sheetname from the file
+
+    temp_sheetname = srcfile.get_sheet_by_name('Daniels Tables')
+    print (temp_sheetname['J6'].internal_value)
+    print (temp_sheetname['C2'].value)
     print ("Filler")#Read from I/J 6 to get VDOT score
+    srcfile.close()
