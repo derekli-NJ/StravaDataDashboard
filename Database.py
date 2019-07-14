29bbf2546b9710c71e1d1ebfedd844b6093d8950
Derek Li <derekgolf72@yahoo.com>
2019-07-14 00:35:25 -0400

    Successfully create database with race data, and is modular so that I can store more attributes as I add more features

---
+++ Database.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+def create_database(vdot_data):
+    sql_create_race_table= """ CREATE TABLE IF NOT EXISTS race(
+                                         id integer PRIMARY KEY,
+                                         distance real,
+                                         time text,
+                                         vdot real,
+                                         date text
+                                     ); """
+
+    connection = sqlite3.connect("race.db")
+    cursor = connection.cursor()
+    cursor.execute(sql_create_race_table)
+
+    #print (vdot_data)
+
+    sql_formatted_data = []
+    
+    for key in vdot_data:
+        #Choosing to use the rounded value entered into vdot calculator (decide if I want to use the exact value later)
+        sql_formatted_data.append((key, vdot_data[key][1]['entered']['distance'], vdot_data[key][1]['entered']['time'], vdot_data[key][1]['vdot'], str(vdot_data[key][0].start_date)))
+
+    print (sql_formatted_data)
+
+    cursor.executemany('''INSERT INTO race(id, distance, time, vdot, date) VALUES(?, ?, ?, ?, ?)''', sql_formatted_data)
+
+    connection.commit()
+    connection.close()
+
--- VDOT.py
+++ VDOT.py
@@ -1,34 +1,39 @@
 import sqlite3
-import openpyxl
+#import openpyxl
 from units import unit
 import units.predefined
 from stravalib import unithelper as uh
 
-import xlwings as xw
+#import xlwings as xw
 import requests
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import json
 import brotli
+#import datetime
 
 def format_data(athlete_info, race_data):
-    #print (race_data)
     race_distances = { "5k" : uh.meter(5000), "8k" : uh.meter(8000), "10k" : uh.meter(10000), "12k" : uh.meter(12000), "15k" : uh.meter(15000), "1/2 Mar" : uh.meter(21097.5), "Marathon" : uh.meter(42195)}
 
     formatted_race_data = [] 
+    
+    #Sorts races into chronological order
+    race_data.sort(key = lambda x : x.start_date)
 
     for race in race_data:
+        #print (race.upload_id)
         for distance in race_distances:
+
+            #race.moving_time = str(race.moving_time).zfill(8)
             if (abs(race_distances[distance] - race.distance) < 0.1 * race.distance):   
                 race.distance = race_distances[distance]
-                # Zfill adds leading 0 if string is less than 8 characters (needed formatting for post request)
-                formatted_race_data.append((race.distance, str(race.moving_time).zfill(8)))
+
+        formatted_race_data.append(race)
+
                 #Need to add way to deal with custom length race later
     return formatted_race_data
-    #do more here
                 
 
 def calculate_vdot(race_data):
-    
     headers = {
         'cookie': '__cfduid=d0b9f95405c5e2d2099437f23da0da88f1561088759; __utmc=140335664; _ga=GA1.2.1431627166.1561088760; __utmz=140335664.1561089351.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); _gid=GA1.2.1220013293.1562985965; _gat=1; __utma=140335664.1431627166.1561088760.1561089351.1562985974.3; __utmt=1; __utmb=140335664.3.9.1562985987153',
         'origin': 'https://runsmartproject.com',
@@ -41,41 +46,51 @@ def calculate_vdot(race_data):
         'authority': 'runsmartproject.com',
         'x-requested-with': 'XMLHttpRequest',
     }
-    count = 0 
+
+    vdot_data = []
+
     for race in race_data:
-        if count == 0:
-            data = {
-              'distance': race[0],
-              'unit': 'm',
-              'time': race[1],
-              'pace': 'empty',
-              'punit': 'mi',
-              'temp': '',
-              'tunit': 'F',
-              'alt': '',
-              'aunit': 'ft',
-              'advtype': 'temperature',
-              'predict': 'true'
-            }
+        data = {
+          'distance': race.distance,
+          'unit': 'm',
+          # Zfill adds leading 0 if string is less than 8 characters (needed formatting for post request)
+          'time': str(race.moving_time).zfill(8),
+          'pace': 'empty',
+          'punit': 'mi',
+          'temp': '',
+          'tunit': 'F',
+          'alt': '',
+          'aunit': 'ft',
+          'advtype': 'temperature',
+          'predict': 'true'
+        }
 
-            #r = requests.get('https://runsmartproject.com/calculator/')
-            response = requests.post('https://runsmartproject.com/vdot/app/api/find_paces', headers=headers, data=data)
-            
-            print (brotli.decompress(response.content))
-            #r = requests.get('https://runsmartproject.com/calculator/')
+        #r = requests.get('https://runsmartproject.com/calculator/')
+        response = requests.post('https://runsmartproject.com/vdot/app/api/find_paces', headers=headers, data=data)
+        
+        json_response = json.loads(brotli.decompress(response.content).decode("ascii"))
+        #print (json_response['entered']['distance'])
+        #print (json_response['entered']['time'])
+        #print (json_response['vdot'])
 
-            soup = BeautifulSoup(response.text, "html.parser")
-            links = soup.find_all("a")
-            tag = soup.find("div", id="vdot")
-        count += 1
+        vdot_data.append((race.upload_id, [race, json_response]))
+
+        #print (type(json_response))
+        #r = requests.get('https://runsmartproject.com/calculator/')
+
+        #soup = BeautifulSoup(response.text, "html.parser")
+        #links = soup.find_all("a")
+        #tag = soup.find("div", id="vdot")
+    formatted_vdot_data = dict(vdot_data)
+    return (formatted_vdot_data)
 
 
-def create_database(data): 
-    sql_create_accounts_table= """ CREATE TABLE IF NOT EXISTS race_data(
-                                         id integer PRIMARY KEY,
-                                         distance decimal,
-                                         time decimal,
-                                     ); """
+#def create_database(data): 
+    #sql_create_accounts_table= """ CREATE TABLE IF NOT EXISTS race_data(
+                                         #id integer PRIMARY KEY,
+                                         #distance decimal,
+                                         #time decimal,
+                                     #); """
     '''
     srcfile = openpyxl.load_workbook('test.xlsm',read_only=False, keep_vba= True)#to open the excel sheet and if it has macros
 
